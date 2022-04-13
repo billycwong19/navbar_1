7286bc7e9e4b257ad6881778d9d757217c3b04d9
Ocelot <100012348+billycwong19@users.noreply.github.com>
2022-04-12 23:51:58 -0500

    updating formulas

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,5 @@
-// Working from W3 example
 var input = document.getElementById("searchBar");
 
-input.addEventListener("keyup", function(event){
-    if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById("searchButton").click();
-    }
-});
-
 var x = 2;
 if (x=2){
     x-1
@@ -21,4 +13,16 @@ var x = 2;
 if (x=2){
     x-1
 }
-var four = 2 + 2;
+var x = 2;
+if (x=2){
+    x-1
+}
+var x = 2;
+if (x=2){
+    x-1
+}
+var x = 2;
+if (x=2){
+    x-1
+}
+
