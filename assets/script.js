94016d771c6fe749e9447f6df181d2ce9bdfa555
billycwong19 <100012348+billycwong19@users.noreply.github.com>
2022-04-12 23:49:16 -0500

    just some changes!

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
-// Working from W3 example
-var input = document.getElementById("searchBar");
-
+input.addEventListener("keyup", function(event){
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("searchButton").click();
+    }
+});
 input.addEventListener("keyup", function(event){
     if (event.key === "Enter") {
         event.preventDefault();
@@ -17,8 +20,6 @@ var x = 2;
 if (x=2){
     x-1
 }
-var x = 2;
-if (x=2){
-    x-1
-}
 var four = 2 + 2;
+
+var input = document.getElementById("searchBar");
